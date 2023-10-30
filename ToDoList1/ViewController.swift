ede91defe74aa39613b18f3f6c99e291f279795f
AnnaZubakin <146950990+AnnaZubakin@users.noreply.github.com>
2023-10-30 20:42:11 +0200

    beginning
    October 30

---
+++ ToDoList1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList1
+//
+//  Created by anna.zubakina on 30/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
